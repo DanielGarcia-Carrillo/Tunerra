6f19448266077e7e3c2954c49e2d4bf42361cbdf
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2013-03-14 00:44:58 -0500

    The login and signup idk

--- tunerra/views.py
+++ tunerra/views.py
@@ -9,16 +9,12 @@ def index(request):
     return render(request,'index.html', RequestContext(request))
 
 class SignupForm(forms.Form):
-    #def __init__(self, *args):
-    #    super(SignupForm, self).__init__(args, auto_id='id_signup_%s')
     # Putting these attrs here suck
-    username = forms.CharField(max_length=30, min_length=5,widget=forms.TextInput(attrs={'required':True,'placeholder':'Username'}))
+    username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'required':True,'placeholder':'Username'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'required':True,'placeholder':'Email'}))
     password = forms.CharField(min_length=8, widget=forms.PasswordInput(attrs={'required':True,'placeholder':'Password'}))
 
 class LoginForm(forms.Form):
-    #def __init__(self, *args):
-    #    super(LoginForm, self).__init__(args, auto_id='id_login_%s')
     username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'required':True,'placeholder':'Username'}))
     password = forms.CharField(min_length=8, widget=forms.PasswordInput(attrs={'required':True,'placeholder':'Password'}))
 
@@ -27,30 +23,36 @@ def welcome(request):
 
 def login_signup(request):
     if request.method == 'POST':
-        # Hacky way to tell which form is being POSTed
-        signup_form = SignupForm(request.POST)
-        login_form = LoginForm(request.POST)
-        if signup_form.is_valid():
-            new_user = User.objects.create_user(
-                username=signup_form.cleaned_data['username'],
-                email=signup_form.cleaned_data['email'],
-                password=signup_form.cleaned_data['password']
-            )
-            #if new_user is None:
-                # TODO do something about this, means that user exists already
-
-            return HttpResponseRedirect('/accounts/welcome')
-        elif login_form.is_valid():
-            user = authenticate(username=login_form.username, password=login_form.password)
-            if user is None:
-                # Return error
-                return HttpResponse()
+        if request.POST.get('form-name') == 'signup':
+            signup_form = SignupForm(request.POST)
+            if signup_form.is_valid():
+                existing_user = User.objects.filter(username=signup_form.cleaned_data['username'])
+                if not existing_user:
+                    new_user = User.objects.create_user(
+                        username=signup_form.cleaned_data['username'],
+                        email=signup_form.cleaned_data['email'],
+                        password=signup_form.cleaned_data['password']
+                    )
+                    return HttpResponseRedirect('/accounts/welcome')
+            return render(request, 'accounts.html', {
+                'signup_form':signup_form,
+                'login_form':LoginForm()
+            })
+        elif request.POST.get('form-name') == 'login':
+            login_form = LoginForm(request.POST)
+            if login_form.is_valid():
+                user = authenticate(username=login_form.cleaned_data['username'], password=login_form.cleaned_data['password'])
+                if user is None:
+                    # Return error
+                    return HttpResponse()
+                else:
+                    # Send to profile
+                    return HttpResponse()
             else:
-                # Send to profile
-                return HttpResponse()
-        else:
-            # TODO make this discriminant of which form was POSTed
-            return render(request, 'accounts.html', {'signup_form':signup_form, 'login_form':login_form})
+                return render(request, 'accounts.html', {
+                    'signup_form':SignupForm(),
+                    'login_form':login_form
+                })
     else:
         return render(request, 'accounts.html', {
             'signup_form': SignupForm(),
