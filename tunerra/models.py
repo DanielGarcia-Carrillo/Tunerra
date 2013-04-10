0294c23b60f57d8cb6c3b245fa18d33ea1e5e2b7
David Eisenberg <eisenbe7@illinois.edu>
2013-04-10 18:18:23 -0500

    model

--- tunerra/models.py
+++ tunerra/models.py
@@ -2,31 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-"""CREATE TABLE tunerra_region
-(
-  name character varying(100) NOT NULL,
-  CONSTRAINT tunerra_region_pkey PRIMARY KEY (name)
-)"""
 class Region(models.Model):
     name = models.CharField(max_length=100, primary_key=True, unique=True)
-
-"""CREATE TABLE tunerra_userpreferences
-(
-  id serial NOT NULL,
-  user_id integer NOT NULL,
-  notify_system character varying(100) NOT NULL,
-  preferred_region_id character varying(100) NOT NULL,
-  preferred_popularity character varying(100) NOT NULL,
-  preferred_genre character varying(100) NOT NULL,
-  CONSTRAINT tunerra_userpreferences_pkey PRIMARY KEY (id),
-  CONSTRAINT tunerra_userpreferences_preferred_region_id_fkey FOREIGN KEY (preferred_region_id)
-      REFERENCES tunerra_region (name) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY DEFERRED,
-  CONSTRAINT tunerra_userpreferences_user_id_fkey FOREIGN KEY (user_id)
-      REFERENCES auth_user (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY DEFERRED,
-  CONSTRAINT tunerra_userpreferences_user_id_key UNIQUE (user_id)
-)"""
 
 class UserPreferences(models.Model):
     user = models.OneToOneField(User)
@@ -34,51 +11,29 @@ class UserPreferences(models.Model):
     preferred_region = models.ForeignKey(Region)
     preferred_popularity = models.CharField(max_length=100)
     preferred_genre = models.CharField(max_length=100)
+    last_fmName = models.CharField(max_length=300)
 
-
-"""
-CREATE TABLE tunerra_song
-(
-  id serial NOT NULL,
-  artist character varying(100) NOT NULL,
-  title character varying(100) NOT NULL,
-  album character varying(100) NOT NULL,
-  year date NOT NULL,
-  CONSTRAINT tunerra_song_pkey PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE tunerra_song
-  OWNER TO main;
-  """
+class Genre(models.Model):
+  name = models.CharField(max_length=100, unique=True)
+  popularity = models.DecimalField(max_digits=10, decimal_places=6)
+  parentGenre = models.ForeignKey('self')
 
 class Song(models.Model):
     artist = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     album = models.CharField(max_length=100, null=True)
     year = models.DateField(auto_now = False)
+    popularity = models.DecimalField(max_digits=10, decimal_places=6)
+    genre = models.ForeignKey(Genre)
     class Meta:
         unique_together=['artist', 'title']
 
+class Post(models.Model):
+  user = models.ForeignKey(User)
+  title = models.CharField(max_length=1500)
+  song = models.ForeignKey(Song)
+  likes = models.IntegerField()
 
-"""CREATE TABLE tunerra_favorites
-(
-  id serial NOT NULL,
-  user_id integer NOT NULL,
-  hotness_level integer NOT NULL,
-  song_id_id integer NOT NULL,
-  play_count integer NOT NULL,
-  last_played timestamp with time zone NOT NULL,
-  CONSTRAINT tunerra_favorites_pkey PRIMARY KEY (id),
-  CONSTRAINT tunerra_favorites_song_id_id_fkey FOREIGN KEY (song_id_id)
-      REFERENCES tunerra_song (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY DEFERRED,
-  CONSTRAINT tunerra_favorites_user_id_fkey FOREIGN KEY (user_id)
-      REFERENCES auth_user (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY DEFERRED
-)
-"""
 class Favorites(models.Model):
     user = models.ForeignKey(User)
     hotness_level = models.IntegerField()
@@ -88,4 +43,6 @@ class Favorites(models.Model):
     class Meta:
         unique_together=['user', 'song_id']
 
-
+class Follows(models.Model):
+  user = models.ForeignKey(User, related_name = 'Follows_source')
+  following = models.ForeignKey(User, related_name = 'Follows_dest')
