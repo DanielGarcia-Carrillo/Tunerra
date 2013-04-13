51ad9242b9d4dcb3b4806ee3a5c9c5f161f49109
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2013-04-13 17:15:52 -0500

    New music data models added

--- tunerra/models.py
+++ tunerra/models.py
@@ -16,17 +16,31 @@ class UserPreferences(models.Model):
 class Genre(models.Model):
   name = models.CharField(max_length=100, unique=True)
   popularity = models.DecimalField(max_digits=10, decimal_places=6)
-  parentGenre = models.ForeignKey('self')
 
 class Song(models.Model):
-    artist = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
-    album = models.CharField(max_length=100, null=True)
-    year = models.DateField(auto_now = False)
+    album = models.ForeignKey(Album)
+    artist = models.ForeignKey(Artist)
     popularity = models.DecimalField(max_digits=10, decimal_places=6)
     genre = models.ForeignKey(Genre)
+    track_number = models.IntegerField(max_length=5)
+    bpm = models.IntegerField(max_length=4)
+    length = models.CharField(max_length=6)
+    provider = models.ForeignKey(MetadataProvider)
+    provider_track_id = models.CharField(max_length=100)
     class Meta:
         unique_together=['artist', 'title']
+
+class Album(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    cover_art_url = models.URLField()
+    year = models.DateField(auto_now=False)
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class MetadataProvider(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
 class Post(models.Model):
   user = models.ForeignKey(User)
