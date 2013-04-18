df2815f7f13ced2eb09add08d8742bb750e19b2f
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2013-04-18 16:59:38 -0500

    Stubbed search view

--- tunerra/search_views.py
+++ tunerra/search_views.py
@@ -1,14 +1,12 @@
 from django.views.generic import View
 from django.shortcuts import render
 from django.template import RequestContext
-from django.http import HttpRequest
 
 class search(View):
     def get(self, request, *args, **kwargs):
-        request = HttpRequest(request)
         if request.user.is_authenticated():
             # return user specific results
-            pass
+            return render(request, 'results.html', RequestContext(request))
         else:
             # Return some generic results
             return render(request, 'results.html', RequestContext(request))
