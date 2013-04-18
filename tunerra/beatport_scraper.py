2a799a1e24ab8fc816b06f34e53956887d0fea51
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2013-04-18 04:55:26 -0500

    The BeatPort scraper now saves progress to a file so that it may restart when interrupted or hits an error

--- tunerra/beatport_scraper.py
+++ tunerra/beatport_scraper.py
@@ -15,11 +15,16 @@ def get_progress():
         f = open('.beatport_scraper_progress', 'r')
     except:
         return 0
-    return int(f.read())
+    # There should only be one number written to the file, trim the newline
+    first_line = f.read().strip()
+    f.close()
+    if first_line.isdigit():
+        return int(first_line)
+    return 0
 
 def write_out_progress(iteration):
     progress_file = open('.beatport_scraper_progress', 'w')
-    progress_file.write(iteration)
+    progress_file.write(str(iteration))
     progress_file.close()
 
 def save_artist(artist_name):
@@ -46,21 +51,71 @@ def save_album(album_name, cover_url, release_year):
     return album_record
 
 def save_genre(genre_name):
-    existing_genre = Genre.objects.filter(name=genre_name)
-    genre_record = None
+    existing_genre = Genre.objects.get(name=genre_name)
     if existing_genre:
-        # There should only be one exact match because name is unique
-        existing_genre = existing_genre[0]
-        # Here I'm defining popularity by the number of tracks with that genre
-        existing_genre.popularity += 1
-        existing_genre.save()
         genre_record = existing_genre
     else:
-        genre_record = Genre(name=genre, popularity=1)
+        genre_record = Genre(name=genre_name, popularity=1)
         genre_record.save()
     return genre_record
 
+def scrape_api_page(page_num):
+    # 150 is the max number of tracks we can get per page
+    request = requests.get('http://api.beatport.com/catalog/3/tracks?perPage=150&page='+str(x)+'&sortBy=trackName')
+    if request.status_code == 200:
+        # Put into db
+        try:
+            req_content = request.json()
+        except:
+            raise Exception("Json decoding failed")
+            # Parsing based on: http://api.beatport.com/catalog-object-structures.html for track
+        results = req_content['results']
+        for track in results:
+            # Some tracks have no artist, we don't want these tracks
+            if len(track['artists']) < 1:
+                continue
+                # Hacky way to deal with tracks with multiple artists, ie just get first artist
+            artist = track['artists'][0]['name']
+            album = track['release']['name']
+            image_url = track['images']['large']['secureUrl']
+            year = track['releaseDate']
+
+            provider_id = track['id']
+            title = track['title']
+            track_number = track['trackNumber']
+            bpm = track['bpm']
+            length = track['length']
+            genre = track['genres'][0]['name'].lower()
+
+            # apparently the following condition is required, you can remove it if you like pain
+            if len(artist)>100 or len(album)>100 or len(title)>100 or len(length) > 6:
+                continue
+            artist_record = save_artist(artist)
+            album_record = save_album(album,image_url,year)
+
+            genre_record = save_genre(genre)
+
+            exact_song_matches = Song.objects.filter(title=title,artist=artist_record)
+            if not exact_song_matches:
+                # Setting 0 popularity because TODO
+                song_record = Song(title=title,
+                                   artist=artist_record,
+                                   album=album_record,
+                                   genre=genre_record,
+                                   popularity=0,
+                                   bpm=bpm,
+                                   length=length,
+                                   track_number=track_number,
+                                   provider=metadata_provider,
+                                   provider_track_id=provider_id
+                )
+                song_record.save()
+
+    else:
+        raise Exception('Bad request at iteration: ' + str(x))
+
 if __name__ == '__main__':
+    import traceback
     # TODO absolute path
     setup_django_env('/Users/Daniel/Tunerra/myproject')
 
@@ -74,59 +129,12 @@ if __name__ == '__main__':
         metadata_provider.save()
 
     initial_page = get_progress()
+
     for x in range(initial_page, 17894):  # 17894 is the last beatport page
-        # 150 is the max number of tracks we can get per page
-        req = requests.get('http://api.beatport.com/catalog/3/tracks?perPage=150&page='+str(x)+'&sortBy=trackName')
-        if req.status_code == 200:
-            # Put into db
-            try:
-                req_content = req.json()
-            except:
-                print "Json decoding failed"
-                break
-            # Parsing based on: http://api.beatport.com/catalog-object-structures.html for track
-            results = req_content['results']
-            for track in results:
-                # Some tracks have no artist or genre, we don't want these tracks
-                if len(track['artists']) < 1:
-                    continue
-                # Hacky way to deal with tracks with multiple artists, ie just get first artist
-                artist = track['artists'][0]['name']
-                album = track['release']['name']
-                image_url = track['images']['large']['secureUrl']
-                year = track['releaseDate']
-
-                id = track['id']
-                title = track['title']
-                track_number = track['trackNumber']
-                bpm = track['bpm']
-                length = track['length']
-                genre = track['genres'][0]['name'].lower()
-
-                # apparently the following condition is required, you can remove it if you like pain
-                if len(artist)>100 or len(album)>100 or len(title)>100 or len(length) > 6:
-                    continue
-                artist_record = save_artist(artist)
-                album_record = save_album(album,image_url,year)
-
-                genre_record = save_genre(genre)
-
-                exact_song_matches = Song.objects.filter(title=title,artist=artist_record)
-                if not exact_song_matches:
-                    # Setting 0 popularity because TODO
-                    song_record = Song(title=title,
-                                       artist=artist_record,
-                                       album=album_record,
-                                       genre=genre_record,
-                                       popularity=0,
-                                       bpm=bpm,
-                                       length=length,
-                                       track_number=track_number,
-                                       provider=metadata_provider,
-                                       provider_track_id=id
-                                       )
-                    song_record.save()
-
-        else:
-            print "Something went wrong at iteration: " + str(x)
+        try:
+            scrape_api_page(x)
+        except Exception as exc:
+            print exc
+            traceback.print_exc()
+            write_out_progress(x)
             break
