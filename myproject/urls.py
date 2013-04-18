57378ca669f57f1df3bc2c5533f558dbe37740a7
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2013-04-18 16:42:20 -0500

    Refactored template urls to use variables rather than hard coding url path

--- myproject/urls.py
+++ myproject/urls.py
@@ -8,16 +8,16 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'tunerra.views.index', name='home'),
-    url(r'^search$', search_views.search.as_view()),
+    url(r'^search$', search_views.search.as_view(), name='search'),
 
-    url(r'^accounts$', 'tunerra.views.login_signup'),
-    url(r'^accounts/welcome$', 'tunerra.views.welcome'),
-    url(r'^accounts/logout$', 'tunerra.views.logout_user'),
+    url(r'^accounts$', 'tunerra.views.login_signup', name='login_signup'),
+    url(r'^accounts/welcome$', 'tunerra.views.welcome', name='welcome'),
+    url(r'^accounts/logout$', 'tunerra.views.logout_user', name='logout'),
     url(r'^accounts/profile/(?P<username>[A-Z|a-z|0-9|+|\-|_|.|@]{1,30})$', profile_views.ProfilePage.as_view()),
     url(r'^login_error$', 'tunerra.views.login_error'),
-    url(r'^accounts/settings$', 'tunerra.views.settings'),
+    url(r'^accounts/settings$', 'tunerra.views.settings', name='settings'),
     url(r'^accounts/facebook_connect', social_views.facebook_connect.as_view()),
-    url(r'^map', 'tunerra.views.map_page'),
+    url(r'^map', 'tunerra.views.map_page', name='map'),
     
     url(r'^channel.html$', 'tunerra.social_views.facebook_api'),
     # Insert RESTful url here for accounts (accounts/[id] or profile/[id]) perhaps?
