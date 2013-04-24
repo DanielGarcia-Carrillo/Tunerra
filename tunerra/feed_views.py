332dfd2db227b8a5409e0b279f7659df68237129
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2013-04-24 18:53:04 -0500

    Made change to lastfm python file for imports and api key and such. Feed page now has like button support via ajax.

--- tunerra/feed_views.py
+++ tunerra/feed_views.py
@@ -5,16 +5,31 @@ from django.contrib.auth import logout
 from django.http import Http404, HttpResponseRedirect
 from tunerra import models
 from django import forms
+from lastFm import getLastFmSong
 
 
 class FeedPage(View):
-    # POSTing to the profile page is literally for making posts on a profile
+    # POSTing to the page is literally for making posts on a profile
     def post(self, request, *args, **kwargs):
         if request.user.is_authenticated():
             form = PostForm(request.POST)
             if form.is_valid():
-                saved_post = models.Post(user=request.user, body=form.cleaned_data['body'], likes=0, type='U')
-                saved_post.save()
+                artist_name = form.cleaned_data['artist']
+                song_title = form.cleaned_data['title']
+
+                try:
+                    artist_record = models.Artist.objects.get(name=artist_name)
+                    song_record = models.Song.objects.get(title=song_title, artist=artist_record)
+                except:
+                    song_record = getLastFmSong(song_title, artist_name)
+
+                if song_record:
+                    saved_post = models.Post(body=form.cleaned_data['body'], song=song_record, user=models.User.objects.get(username=request.user.username))
+                    saved_post.save()
+                else:
+                    # TODO ALLL OF THE ERRORS
+                    pass
+
                 profile_context = self.page_context(request)
                 profile_context.update({'post_form': PostForm()})
                 return render(request, 'feed.html', RequestContext(request, profile_context))
@@ -37,13 +52,14 @@ class FeedPage(View):
             raise Http404
 
     def page_context(self, request):
-        # TODO get all posts that are music recommendations, follow recommendations and
         # Order posts by descending creation_time order and get 10 latest TODO respond to pagination, ie get later pages on request
         music_recs = models.MusicRecommendation.objects.filter(user=request.user).order_by('-creation_time')[:10]
         follow_recs = models.FollowRecommendation.objects.filter(user=request.user).order_by('-creation_time')[:10]
         follows = models.Follows.objects.filter(user=request.user)
+        self_and_following = [f.following for f in follows]
+        self_and_following.append(models.User.objects.get(username=request.user.username))
         # essentially an inner join: user__in
-        feed_posts = models.Post.objects.filter(user__in=[f.following for f in follows]).order_by('-creation_time')[:10]
+        feed_posts = models.Post.objects.filter(user__in=self_and_following).order_by('-creation_time')[:10]
 
         return {'feed_posts': feed_posts, 'music_recs': music_recs, 'follow_recs': follow_recs}
 
