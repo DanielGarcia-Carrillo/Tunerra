6231bf0da63629446e89de6d37aef8f60ce62959
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2013-04-25 01:52:41 -0500

    recomendation view working

--- tunerra/recommendation_views.py
+++ tunerra/recommendation_views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.views.generic import View
 import requests
 import json
+import urllib2
+import time
 from recommendations import recommendSong, recommendUser
 
 lastFmLink_Track = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=64e400a9de3b4287b61df31a91237cb3&artist=!&track=$&format=json'
@@ -26,18 +28,9 @@ class MusicRecommendation(View):
         for x in range(3):
             song = recommendSong(request.user)
             recommendation = models.MusicRecommendation(user=request.user, song=song)
+            recommendation.save()
             cover_url = song.album.cover_art_url
-            provider_url = ""
-            if song.provider == 'Beatport':
-                r = requests.get('http://api.beatport.com/catalog/3/beatport/track?id='+ str(song.provider_track_id))
-                if r.status_code == 200:
-                    r_content = r.json()
-                    provider_url = r_content['results'][0]['slug']
-            else:
-                r = requests.get(lastFmLink_Track.replace('!', song.artist.name).replace('$', song.title).replace(' ', ''))
-                if r.status_code == 200:
-                    pass
-
+            provider_url = get_provider_link(song)
 
             rec_songs.append({
                 "cover_url": cover_url,
@@ -48,3 +41,25 @@ class MusicRecommendation(View):
             })
 
         return HttpResponse(content=json.dumps(rec_songs), status=200)
+
+
+def get_provider_link(song):
+    provider_url = ""
+    beatport_provider = models.MetadataProvider.objects.get(name='Beatport')
+    if song.provider == beatport_provider:
+        r = requests.get('http://api.beatport.com/catalog/3/beatport/track?id='+ str(song.provider_track_id))
+        if r.status_code == 200:
+            r_content = r.json()
+            provider_url = 'http://beatport.com/track/' + r_content['results']['track']['slug'] + '/' + str(song.provider_track_id)
+    else:
+        url = lastFmLink_Track.replace('!', song.artist.name.replace(' ', '+')).replace('$', song.title.replace(' ', '+')).replace(' ', '')
+        req = urllib2.Request(url)
+        head = urllib2.build_opener()
+        try:
+            time.sleep(1)
+            jsonSong = json.load(head.open(req))
+            provider_url = jsonSong['track']['url']
+        except:
+            provider_url = ""
+
+    return provider_url
